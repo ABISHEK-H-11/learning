5ce5f6251a12ff96f429cda51dea4364fa7d40b6
ABISHEK-H-11 <abisheka1342003@gmail.com>
2026-09-01 19:33:06 +0530

    for PR request

--- src/main/java/com/learningMicroservice/order_service/dumy/dummy.java
+++ src/main/java/com/learningMicroservice/order_service/dumy/dummy.java
@@ -1,4 +1,13 @@
 package com.learningMicroservice.order_service.dumy;
 
 public class dummy {
+    final Integer a = 1000000;
+    final Integer b = 1000000;
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int adds(int a, int b) {
+        return a + b;
+    }
+    
 }
